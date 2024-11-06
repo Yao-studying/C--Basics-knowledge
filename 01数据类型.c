fc913bf7a9a5ae25455d4068f511593c0b9cb12d
Yao-studying <3317754104@qq.com>
2024-11-06 15:00:08 +0800

    01数据类型改逗号

---
+++ 01数据类型.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf("char的大小是%zu个字节,%d比特位\n", sizeof(char), 8 * sizeof(char));
+    printf("short的大小是%zu个字节,%d比特位\n", sizeof(short), 8 * sizeof(short));
+    printf("int的大小是%zu个字节,%d比特位\n", sizeof(int), 8 * sizeof(int));
+    printf("long的大小是%zu个字节,%d比特位\n", sizeof(long), 8 * sizeof(long));
+    printf("long long的大小是%zu个字节,%d比特位\n", sizeof(long long), 8 * sizeof(long long));
+    printf("float的大小是%zu个字节,%zu比特位\n", sizeof(float), 8 * sizeof(float));
+    printf("double的大小是%zu个字节,%d比特位\n", sizeof(double), 8 * sizeof(double));
+}
--- test.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main()
-{
-  printf("欢迎开始25考研,关闭VPN测试");
-  return 0;
-}
