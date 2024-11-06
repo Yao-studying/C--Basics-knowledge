7e5fc1af628f48cf5b62cc94ba3b910a62304bc9
Yao-studying <3317754104@qq.com>
2024-11-06 17:56:09 +0800

    新增四节

--- 01数据类型.c
+++ 01数据类型.c
@@ -8,4 +8,6 @@ int main()
     printf("long long的大小是%zu个字节,%d比特位\n", sizeof(long long), 8 * sizeof(long long));
     printf("float的大小是%zu个字节,%zu比特位\n", sizeof(float), 8 * sizeof(float));
     printf("double的大小是%zu个字节,%d比特位\n", sizeof(double), 8 * sizeof(double));
+
+    return 0;
 }
---
+++ 03输入与输出.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int a = 1;
+int b = 2;
+
+int main()
+{
+
+    // 输入          (经典错误！！scanf("请输入a,b的值%d %d", &a, &b);)
+    printf("请输入a,b的值");
+    scanf("%d %d", &a, &b);
+
+    // 运算
+    int sum = a + b;
+
+    // 输出
+    printf("a,b相加为%d", sum);
+    
+    return 0;
+}
